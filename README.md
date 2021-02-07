b543a8f70b196a890b923af1b8a1d5446911de9a
Junseok <js940922@gmail.com>
2021-02-07 23:35:29 +0900

    스프링 프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,13 @@
+# 인프런 스프링 입문 강의 실습
+
+----
+
+## 프로젝트 만들기
+
+* 스프링 프로젝트 생성 : https://start.spring.io/
+* 의존성 :
+> 1. Spring Web
+>> Web 서비스를 위해 사용
+
+> 2. Thymeleaf
+>> Web에서 HTML 템플릿 출력에 사용
---
+++ src/test/java/kr/phodobit/springbootstart/SpringbootstartApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.phodobit.springbootstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
